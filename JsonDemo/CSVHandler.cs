9a87c83a79d9611beaf482d985244adc299fe559
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-26 20:31:34 +0530

    [Sai] Reading and writing to a CSv file and Json file

---
+++ JsonDemo/CSVHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using System.Globalization;
+
+namespace CSVIO
+{
+    public class CSVHandler
+    {
+        public static void WorkWithCSV()
+        {
+            string importFilePath = @"C:\Users\saiku\source\repos\JsonDemo\Utility\SampleData.csv";
+            string exportFilePath = @"C:\Users\saiku\source\repos\JsonDemo\Utility\ExportData.csv";
+            using (var reader = new StreamReader(importFilePath)) 
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Reading Data ......\n");
+
+                foreach(AddressData address in records)
+                {
+                    Console.WriteLine(" " + address.FirstName);
+                    Console.WriteLine(" " + address.LastName);
+                    Console.WriteLine(" " + address.Address);
+                    Console.WriteLine(" " + address.City);
+                    Console.WriteLine(" " + address.State);
+                    Console.WriteLine(" " + address.Code+"\n");
+
+                }
+                Console.WriteLine("Readed Data successFully");
+
+                Console.WriteLine("Writing Data:");
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+                Console.WriteLine("Writed Data successfully:");
+            }
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ JsonDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSVIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //CSVHandler.WorkWithCSV();
+            ReadFromCsv_WriteToJson.JsonWriter();
+
+        }
+    }
+}
